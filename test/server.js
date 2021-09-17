b195f9485ff1468052bcd44b3d25dac046192a39
vgvishal538 <vgvishal538@gmail.com>
2021-09-17 12:37:13 +0530

    routes added

--- test/server.js
+++ test/server.js
@@ -1,6 +1,22 @@
 const express = require("express"),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+  mongoose = require("mongoose"),
+  Task = require("./api/models/todoListModel"),
+  bodyParser = require("body-parser");
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/Tododb");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.use(function(req, res) {
+//   res.status(404).send({url: req.originalUrl + ' not found'})
+// });
+
+var routes = require("./api/routes/todoListRoutes"); //importing route
+routes(app);
 
 app.listen(port);
 
